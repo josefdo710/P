0dfbc0a44c703c828a45e0536de38375f094b237
josefdo710 <josefdo710@gmail.com>
2022-11-23 19:52:28 -0500

    update

---
+++ js/main.js
@@ -0,0 +1,11 @@
+function load3Dselected() {
+    location.href = "./views/pagina-2.html"; 
+}
+function mySelecyPlayer() {
+    initScene();
+    animate();
+}
+function initScene() {
+    initBasicElements(); // Scene, Camera and Render
+    createFistModel("./images/humanoide/","humanoide.mtl","humanoide.obj");
+}
